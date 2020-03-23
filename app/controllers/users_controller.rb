8db5fdf8a24dcf18f588cd09d5610306ceb5b551
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-03-23 18:29:27 -0400

    feat: add users views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,27 @@
 class UsersController < ApplicationController
 
-	def show
-
+	def index
+		set_user_instance
+		redirect_to user_path(@user)
 	end
-	
+
+    def show
+        set_user_instance
+	end
+
+	def edit
+		set_user_instance
+	end
+
+	def update
+		set_user_instance
+		@user.update(strong_params)
+		redirect_to edit_user_path(@user)
+	end
+
+	private
+
+	def strong_params
+		params.require(:user).permit(:name, :home_address, :work_address)
+	end
 end
