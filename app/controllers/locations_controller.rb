4b792960da2ca17c2046adacca4f40c64e23a3d7
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-03-23 19:23:48 -0400

    feat: add basic location page

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,12 @@
 class LocationsController < ApplicationController
+	def show
+		set_user_instance
+		set_instance
+	end
+
+	private
+
+	def set_instance
+		@location = Location.find(params[:id])
+	end	
 end
