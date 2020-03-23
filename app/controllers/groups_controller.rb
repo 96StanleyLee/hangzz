dc445928d0bace12f84e1fd802276f4da1a7297a
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-03-23 18:55:26 -0400

    feat: add group show page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,12 @@
 class GroupsController < ApplicationController
+	def show
+		set_user_instance
+		set_instance
+	end
 
+	private
 
-
-    
+	def set_instance
+		@group = Group.find(params[:id])
+	end
 end
