a9f9342cb08f6a4a26b31b2d257fbcf6eb71d450
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-03-23 18:30:54 -0400

    feat: add ability to join group

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,2 +1,21 @@
 class MembershipsController < ApplicationController
+    
+    def new
+        @user = User.find(params[:user_id])
+        @group_list = Group.all.select do |group|
+            !@user.groups.include?(group)
+        end     
+        @membership = Membership.new
+    end 
+
+    def create 
+    end 
+
+
+
+    private 
+
+    def strong_params
+        params.restrict(:membership).permit(:user_id,:group_id)
+    end 
 end
