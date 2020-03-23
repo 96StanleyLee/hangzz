b2d4a330ccbf424e8679b9b1998742e698ad3ad5
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-03-23 18:28:48 -0400

    feat: add meetup show page

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -1,2 +1,12 @@
 class MeetupsController < ApplicationController
+	def show
+		set_user_instance
+		set_instance
+	end
+
+	private
+
+	def set_instance
+		@meetup = Meetup.find(params[:id])
+	end
 end
