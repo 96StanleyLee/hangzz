ef332400a6357ceac3b1c34c64a86abf0b979375
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-03-23 18:31:38 -0400

    feat: add .members alias method

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,11 @@
 class Group < ApplicationRecord
-	has_many :users
-	has_many :locations, through: :users
+	has_many :memberships
+	has_many :users, through: :memberships
 
 	has_many :meetups
 	has_many :locations, through: :meetups
+
+	def members
+		self.users
+	end
 end
