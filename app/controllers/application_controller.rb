1d293e0fb55405a8d3e3cc1d70ae3b1d523ad823
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-03-24 19:11:32 -0400

    feat: add auth methods and before_actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,24 @@
 class ApplicationController < ActionController::Base
 
+	helper_method :logged_in?, :current_user
+	
+	before_action :authorized
+	before_action :current_user
+
 	private
 
-	def set_user_instance
-		@user = User.find(1)
+	def current_user
+		if session[:user_id]
+			@user = User.find(session[:user_id])
+		end
+	end
+
+	def authorized
+		redirect_to login_path unless logged_in?
+	end
+
+	# helper method for #authorized
+	def logged_in?
+		!!current_user
 	end
 end
