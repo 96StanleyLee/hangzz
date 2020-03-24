5d7e9de70cc93a495c6c8d117654c7aa4632ee00
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-03-24 19:06:10 -0400

    feat: add password macro and set up validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+	has_secure_password
+	
 	has_many :memberships
 	has_many :groups, through: :memberships
+	validates :password_digest, presence: true
+	validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true, presence: true
+	validates :name, presence: true
+
 end
