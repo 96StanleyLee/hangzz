45e85ec2b4dec5eee110c0f1179bace9bcaf76c1
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-03-26 18:00:54 -0400

    feat: add error message for wrong password

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,6 +12,7 @@ class SessionsController < ApplicationController
 			session[:user_id] = @user.id
 			redirect_to root_path
 		else
+			flash[:alert] = ['Email or password is incorrect']
 			redirect_to login_path
 		end 	
 	end
