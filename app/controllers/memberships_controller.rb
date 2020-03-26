907267175337d9fccb1d67b8401b3f476fd5abe5
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-03-26 10:03:34 -0400

    fix: update items for friendly urls

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,16 +1,13 @@
 class MembershipsController < ApplicationController
     
     def new
-        @user = User.find_by_slug(params[:slug])
-        @group_list = Group.all.select do |group|
-            !@user.groups.include?(group)
-        end     
+        @group_list = Group.all - @user.groups
         @membership = Membership.new
     end 
 
     def create 
         @group = Group.find(params[:membership][:group_id])
-        Membership.create(user_id: params[:user_id], group_id: params[:membership][:group_id])
+        Membership.create(user: @user, group_id: params[:membership][:group_id])
         redirect_to @group
     end 
 
