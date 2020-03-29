b7f4b9cd3e685d8326e62e88b9987a4ace9aee2e
Ben Hickson <11274310+benhickson@users.noreply.github.com>
2020-03-29 14:48:49 -0400

    fix: typos in capistrano settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:benhickson/hangzz.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/deploy/#{fetch :application}"
+set :deploy_to, "/home/ben/#{fetch :application}"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server 'hangzz.benhickson.com', user: 'deploy', roles: %w{app db web}
+server 'hangzz.benhickson.com', user: 'ben', roles: %w{app db web}
 
 # role-based syntax
 # ==================
